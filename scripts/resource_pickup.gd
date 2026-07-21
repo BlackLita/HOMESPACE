915d10b96c93d3afcb3b118785b2c8d5350717b6
BlackLit <likorik237@gmail.com>
2026-07-22 02:22:19 +1000

    added new solution

--- scripts/resource_pickup.gd
+++ scripts/resource_pickup.gd
@@ -1,8 +1,8 @@
 extends Node3D
 class_name ResourcePickup
 
-@export var resource: String
-@export var amount: float
+@export_enum("metal", "electronics", "fuel") var resource: String
+@export_range(0.0, 25.0, 1.0) var amount: float
 	
 func interact(rm: ResourceManager):
 	rm.add_resource(resource, amount)
